f07042f49993c3527e1c6de707f951fe8bfae16b
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2017-10-14 22:05:26 +0300

    DELETE and DELETE tests /todos/:id

--- server/server.js
+++ server/server.js
@@ -47,6 +47,21 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
+
 app.listen(3000, () => {
     console.log("app started on port 3000");
 });
